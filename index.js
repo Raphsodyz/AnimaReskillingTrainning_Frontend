ec3337e5915d74860165e4dbd72246b730969f99
Raphsodyz <arthurvsrll@gmail.com>
2021-12-15 21:01:47 -0300

    adiçoes aula 2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function carregamentoPagina(){
-    alert("Abriu!");
-}
+// function carregamentoPagina() {
+//   alert("Abriu!");
+// }
+
+$(() => {
+    //Código executado no carregamento da página
+    //alert("Abriu!");
+
+    $("#btnSalvar").click(()=> {
+        console.log("Função click funcionou");
+    })
+})
