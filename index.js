049ba38203f95e669281c6e4fe406a73eb8ad871
Raphsodyz <arthurvsrll@gmail.com>
2021-12-16 21:06:36 -0300

    commit aula 3 JS

--- index.js
+++ index.js
@@ -2,11 +2,53 @@
 //   alert("Abriu!");
 // }
 
+function populaTabela(){
+    if(localStorage.getItem("produtos")){
+        let produtos = JSON.parse(localStorage.getItem("produtos"));
+
+        $("#tblProdutos tbody").html("");
+
+        produtos.forEach((produtos) => {
+            $("#tblProdutos tbody").append(`<tr>
+                <td>${produtos.nome}</td>
+                <td>${produtos.quantidade}</td>
+                <td>${produtos.valor}</td>
+                <td style="width: 50px"><button type="button" class="btn btn-info">E</button></td>
+                <td style="width: 50px"><button type="button" class="btn btn-danger">D</button></td>
+            </tr>`)
+        });
+}
+}
+
 $(() => {
     //Código executado no carregamento da página
-    //alert("Abriu!");
+    populaTabela();
 
-    $("#btnSalvar").click(()=> {
-        console.log("Função click funcionou");
+    $("#btnSalvar").click(() => {
+        //console.log("Função click funcionou");
+        // localStorage.setItem("key", "efetivo valor armazenado");
+
+        let produto = {}
+        produto.nome = $("#nmProduto").val();
+        produto.quantidade = $("#quantidade").val();
+        produto.valor = $("#vlrProduto").val();
+
+        let listaProdutos = [];
+
+        if(localStorage.getItem("produtos")){
+            listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+        }
+
+        listaProdutos.push(produto);
+
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos));
+
+        alert("Dados salvos com sucesso!");
+
+        $("#nmProduto").val("");
+        $("#quantidade").val("");
+        $("#vlrProduto").val("");
+
+        populaTabela();
     })
 })
